92c8ae2405aedc2677095d0a86801e9364d2c4d7
Maxim Kapelina <mkapelina@gmail.com>
2020-06-20 20:20:32 -0400

    implemented sub-component view!

--- src/backend/routes/api/userProjects.js
+++ src/backend/routes/api/userProjects.js
@@ -21,28 +21,34 @@ const userAPI = (app) => {
     });
 
     app.post('/addComponent', function (req, res) {
-        let found;
         var name = req.body.name;
         var desc = req.body.desc;
+        var parentCompName = req.body.compName
 
         var proj = new Project(req.body.projectName, '');
         var obj = new Comp(name, desc);
-        found = defUser.getProject(proj).addComp(obj);
-        found ? res.json(json_success) : res.json(json_not_found);
 
-    });
-
-    app.post('/addSubcomponent', function (req, res) {
-        let found;
-        var name = req.body.name;
-        var desc = req.body.desc;
-
-        var proj = new Project(req.body.projectName, '');
-        var comp = new Comp(req.body.compName, '');
-        var obj = new Comp(name, desc);
-        found = defUser.getProject(proj).getComp(comp).addSubComp(obj);
-        found ? res.json(json_success) : res.json(json_not_found);
-
+        var userProject = defUser.getProject(proj);
+        if (!userProject) {
+            res.json(json_not_found);
+        }
+        else {
+            if (!parentCompName) {
+                userProject.addComp(obj);
+                res.json(json_success);
+            }
+            else {
+                var parentComp = new Comp(parentCompName, '');
+                var userComp = userProject.getComp(parentComp);
+                if (!userComp) {
+                    res.json(json_not_found);
+                }
+                else {
+                    userComp.addSubComp(obj);
+                    res.json(json_success);
+                }
+            }
+        }
     });
 
     app.post('/addStep', function (req, res) {
@@ -70,18 +76,29 @@ const userAPI = (app) => {
 
         var proj = new Project(req.body.projectName, '');
         var obj = new Comp(name, '');
-        defUser.getProject(proj).removeComp(obj);
-        res.json(json_success);
-    });
+        var userProject = defUser.getProject(proj);
+        var parentCompName = req.body.compName;
 
-    app.post('/deleteSubcomponent', function (req, res) {
-        var name = req.body.name;
-
-        var proj = new Project(req.body.projectName, '');
-        var comp = new Comp(req.body.compName, '');
-        var obj = new Comp(name, '');
-        defUser.getProject(proj).getComp(comp).removeSubComp(obj);
-        res.json(json_success);
+        if (!userProject) {
+            res.json(json_not_found);
+        }
+        else {
+            if (!parentCompName) {
+                userProject.removeComp(obj);
+                res.json(json_success);
+            }
+            else {
+                var parentComp = new Comp(parentCompName, '');
+                var userComp = userProject.getComp(parentComp);
+                if (!userComp) {
+                    res.json(json_not_found);
+                }
+                else {
+                    userComp.removeSubComp(obj);
+                    res.json(json_success);
+                }
+            }
+        }
     });
 
     app.post('/deleteStep', function (req, res) {
@@ -117,18 +134,6 @@ const userAPI = (app) => {
         res.json(json_success);
     });
 
-    app.post('/modifySubcomponent', function (req, res) {
-        var name = req.body.name;
-        var desc = req.body.desc;
-
-        var proj = new Project(req.body.projectName, '');
-        var comp = new Comp(req.body.compName, '');
-        var userComp = defUser.getProject(proj).getComp(comp);
-        userComp.name = name;
-        userComp.desc = desc;
-        res.json(json_success);
-    });
-
     app.post('/modifyStep', function (req, res) {
         var name = req.body.name;
         var desc = req.body.desc;
@@ -142,7 +147,7 @@ const userAPI = (app) => {
         res.json(json_success);
     });
 
-    app.post('/getProject', function(req, res){
+    app.post('/getProject', function (req, res) {
         var json = {};
         var projName = req.body.name;
 
@@ -168,76 +173,85 @@ const userAPI = (app) => {
         }
     });
 
-    app.post('/getComponent', function(req, res){
-        let comp;
-        let obj; 
-
-        var json = {};
-        var projName = req.body.name;
-
-        if (!projName) {
-            json["status"] = 400;
-            json["message"] = 'request body must include a project name';
-            res.json(json);
-        }
-        else {
-            var project = new Project(projName, '');
-            var userProject = defUser.getProject(project);
-
-            if (!userProject) {
-                json["status"] = 404;
-                json['message'] = `project ${projName} not found`
-                res.json(json)
-            }
-            else {
-                comp = req.body.compName;
-                obj = new Comp(comp, '');
-                json = userProject.getComp(obj).jsonify();
-                json['status'] = 200;
-            }
-            res.json(json);
-        }
-    });
-
-    app.post('/getSteps', function(req, res){
-        let comp;
-        let obj; 
-        
-        var json = {};
-        var projName = req.body.name;
-
-        if (!projName) {
-            json["status"] = 400;
-            json["message"] = 'request body must include a project name';
-            res.json(json);
-        }
-        else {
-            var project = new Project(projName, '');
-            var userProject = defUser.getProject(project);
-
-            if (!userProject) {
-                json["status"] = 404;
-                json['message'] = `project ${projName} not found`
-                res.json(json)
-            }
-            else {
-                comp = req.body.compName;
-                obj = new Comp(comp, '');
-                json = userProject.getComp(obj).jsonify();
-                json = { 'steps': json.steps };
-                json['status'] = 200;
-            }
-            res.json(json);
-        }
-    });
-
-    app.post('/getProject', function (req, res) {
+    app.post('/getComponent', function (req, res) {
         let comp;
         let obj;
 
         var json = {};
         var projName = req.body.name;
-        var option = req.body.option;
+
+        if (!projName) {
+            json["status"] = 400;
+            json["message"] = 'request body must include a project name';
+        }
+        else {
+            var project = new Project(projName, '');
+            var userProject = defUser.getProject(project);
+
+            if (!userProject) {
+                json["status"] = 404;
+                json['message'] = `project ${projName} not found`
+            }
+            else {
+                comp = req.body.compName;
+                obj = new Comp(comp, '');
+                var userComp = userProject.getComp(obj);
+                if (!userComp) {
+                    json['status'] = 404;
+                    json['message'] = `project ${comp} not found`
+                }
+                else {
+                    json = userComp.jsonify();
+                    json['status'] = 200;
+                }
+            }
+        }
+        res.json(json);
+    });
+
+    app.post('/getSubComponents', function (req, res) {
+        var json = {};
+
+        var projName = req.body.name;
+        var compName = req.body.compName;
+
+        if (!projName || !compName) {
+            json['status'] = 400;
+            json['message'] = 'request body must include project name and parent component name';
+            res.json(json);
+        }
+        else {
+            var project = new Project(projName, '');
+            var userProject = defUser.getProject(project);
+
+            if (!userProject) {
+                json["status"] = 404;
+                json['message'] = `project ${projName} not found`
+                res.json(json)
+            }
+            else {
+                var comp = new Comp(compName, '');
+                var userComp = userProject.getComp(comp);
+                if (!userComp) {
+                    json["status"] = 404;
+                    json['message'] = `component ${compName} not found`
+                }
+                else {
+                    json = userComp.jsonify();
+                    json = { 'subComponents': json.subComponents };
+                    json['status'] = 200;
+                }
+            }
+            res.json(json);
+
+        }
+    });
+
+    app.post('/getSteps', function (req, res) {
+        var json = {};
+
+        var projName = req.body.name;
+        var compName = req.body.compName;
 
         if (!projName) {
             json["status"] = 400;
@@ -254,33 +268,13 @@ const userAPI = (app) => {
                 res.json(json)
             }
             else {
-                switch (option) {
-                    case 'PROJECT':
-                        json = userProject.jsonify()
-                        json['status'] = 200;
-                        break;
-                    case 'COMPONENTS':
-                        comp = req.body.compName;
-                        obj = new Comp(comp, '');
-                        json = userProject.getComp(obj).jsonify();
-                        json['status'] = 200;
-                        break;
-                    case 'STEPS':
-                        comp = req.body.compName;
-                        obj = new Comp(comp, '');
-                        json = userProject.getComp(obj).jsonify();
-                        json = { 'steps': json.steps };
-                        json['status'] = 200;
-                        break;
-                    default:
-                        json["status"] = 400;
-                        json["message"] = 'invalid option type'
-                        break;
-                }
-                res.json(json);
+                var comp = new Comp(compName, '');
+                json = userProject.getComp(comp).jsonify();
+                json = { 'steps': json.steps };
+                json['status'] = 200;
             }
+            res.json(json);
         }
-
     });
 
     app.get('/getProjects', function (req, res) {
